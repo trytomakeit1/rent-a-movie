c6f473265b007cb52930132cc40028647f4fc0cc
trytomakeit1 <trytomakeit1@gmail.com>
2020-10-10 20:53:50 +0330

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MovieCollection from './components/MovieCollection';
+
+function App() {
+  return (
+    <div className="App">
+      <MovieCollection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MovieCollection.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const MovieCollection = () => {
+
+    return <h2>Movie Collection</h2>
+}
+
+export default MovieCollection;
